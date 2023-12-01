4632691867dd3d09e56fdb8c6f7c086e939ace1a
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-01 08:41:37 -0500

    Delete gloabl variables for scores and choices array

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,9 +2,9 @@
 PSEUDOCODE
 
 STEP I: Global Variable Init
-Create array with rock, paper, scissors
-Create variable for computer score
-Create variable for player score
+Create array with rock, paper, scissors [DONE]
+Create variable for computer score [DONE]
+Create variable for player score [DONE]
 
 On initial start... ask player if they want to play a game?
     If yes, proceed with new game
@@ -29,7 +29,7 @@ Print message with win state and score
 
 STEP II.I: Determine winner function
 Take computer choice and player choice
-If player player choice === computer choice then tie
+If player player choice === computer choice then tie and start a new game
     If player choice === rock and computer choice === scissors then increment player score by 1
         else increment computer score by 1
     Else if player choice === scissors and computer choice === paper then increment player score by 1
@@ -45,9 +45,7 @@ Ask player if they want to play again
             If they say yes, new game
             Else initialize a while loop with "How about a new game now?" until answer is yes
         Else initialize while loop with "Are you sure you don't want to play?" until answer is no
-*/
 
-// Initialize global variables
-let choices = ['rock', 'paper', 'scissors'];
-let computerScore = 0;
-let playerScore = 0;
+NOTE: Read the Odin Project instructions and they suggested not to overcomplicate this one... save the work for portfolio projects
+With that in mind, just gonna follow the instructions but I'll leave the pseudocode in case I come back to this
+*/
